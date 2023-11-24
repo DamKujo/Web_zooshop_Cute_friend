7d09438b8d661ae3a7db4e236e263afe647a6051
DamKujo <shurupov.96@list.ru>
2023-11-24 18:49:29 +0300

    create main
    создано поле поиска, карточки товаров, кнопки добавить в корзину и избранное

--- shop/src/App.js
+++ shop/src/App.js
@@ -6,14 +6,14 @@ export default function App() {
           <img width={40} height={40} src="./../images/logo/cat.svg"/>
           <div>
             <h3 className="text-uppercase">Милый питомец</h3>
-            <p>Больше, чем друг</p>
+            <p className="opacity-5">Больше, чем друг</p>
           </div>
         </div>
         
           <ul className="d-flex">
             <li className="mr-30">
               <img width={18} height={18} src="./../images/box.svg"/>
-              <span>500$</span>
+              <span>$ 500</span>
             </li>
             <li>
               <img width={18} height={18} src="./../images/user.svg"/>
@@ -22,7 +22,70 @@ export default function App() {
       
       </header>
       <div className="content p-40">
-        <h1 className="">В наличии</h1>
+        <div className="d-flex align-center justify-between mb-40">
+          <h1 className="">В наличии</h1>
+          <div className="search-block d-flex">
+            <img src="./../images/Search.svg" alt="Search"/>
+            <input placeholder="Поиск..."/>
+          </div>
+        </div>
+        <div className="d-flex">
+          <div className="card">
+            <div className="favorite"> 
+              <img src="./../images/heart-unlike.svg" alt="Unlike"/>
+            </div>
+            <img width={127} height={160} src="./../images/items/dogs/korgi.avif" alt=""/>
+            <h5>Щенок "Корги"</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>$ 599</b>
+              </div>
+              <button>
+                <img width={11} height={11} src="./../images/Plus!.svg" alt=""/>
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img width={127} height={160} src="./../images/items/cats/sfinks.jpg" alt=""/>
+            <h5>Котенок "Сфинкс"</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>$ 499</b>
+              </div>
+              <button>
+                <img width={11} height={11} src="./../images/Plus!.svg" alt=""/>
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img width={127} height={160} src="./../images/items/rabbits/rabbit.jpg" alt=""/>
+            <h5>Кроллик "Вислоухий"</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>$ 399</b>
+              </div>
+              <button>
+                <img width={11} height={11} src="./../images/Plus!.svg" alt=""/>
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img width={157} height={160} src="./../images/items/dogs/ovcharka.jpg" alt=""/>
+            <h5>Щенок "Овчарка"</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>$ 599</b>
+              </div>
+              <button>
+                <img width={11} height={11} src="./../images/Plus!.svg" alt=""/>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
